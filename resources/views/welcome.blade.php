9b200cdaeac892702380564697bb3f672b694760
rtd21 <36414175+rtd21@users.noreply.github.com>
2018-08-04 12:51:26 +0400

    first Lara commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+    @foreach($tasks as $task)
+        <li>{{$task->body}}</li>
+    @endforeach
+</ul>
+</body>
+</html>
