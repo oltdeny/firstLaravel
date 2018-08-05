34e693d9b0b9f2c114dd7d63e83fcd85685f2926
rtd21 <36414175+rtd21@users.noreply.github.com>
2018-08-05 22:19:15 +0400

    Try to use models, controlers, migration...

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
+//Route::get('/tasks', function () {
+//    // $tasks = DB::table('tasks')->get();
+//    // $tasks = App\Task::all();
+//    $tasks = App\Task::incomplete();
+//    return view('tasks.index', compact('tasks'));
+//});
+//
+//Route::get('/tasks/{task}', function ($id) {
+//    // $task = DB::table('tasks')->find($id);
+//    $task = App\Task::find($id);
+//    return view('tasks.show', compact('task'));
+//});
